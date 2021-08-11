a16e3b513aa765c238baac613272f9a6199733df
Andrey <pavlovskyiandrii@gmail.com>
2021-08-11 09:11:00 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+"use strict";
+// const result = confirm("Are you hear?");
+// console.log(result);
+// const answer = prompt("Вам есть 18?", "18");
+// console.log(answer);
+//const answers = [];
+
+//answers[0] = prompt('как вас зовут?', '');
+//answers[1] = prompt('Фамилия ?', '');
+//answers[2] = prompt('Сколько лет?', '');
+
+//console.log(typeof(answers));
+//console.log(typeof(nall));
+
+//const category = 'toys';
+//console.log(`https://someurl.com/${category}/5`);
+
+//console.log('arr'+" - object");
+//console.log(4 +"9");
+
+let incr = 10,
+decr = 10;
+
+//++incr;
+//--decr;
+
+// console.log (incr++);
+// console.log (decr--);
+// console.log(5%2);
+
+console.log (2 + 2 *2 == '6');
+
+const isChercked = true,
+      isClose = false;
+ console.log(isChercked || !isClose); 
